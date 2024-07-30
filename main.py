3485046e7cc557bb1c4a8f561c67bb46234f0cda
github_username_here <mesherbetmolla@gmail.com>
2024-07-29 20:56:34 -0400

    Count characters function done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
 
 
 def get_book_text(path):
@@ -9,13 +9,48 @@ def get_book_text(path):
         return f.read()
 
 def count_words(text):
-    words = text.split()
-    count = 0
-    for word in range(len(words)):
-        count += 1
+    return len(text)
         
-    return count
-        
+def count_chr(text):
+    char_map = {
+        'a':0,
+        'b':0,
+        'c':0,
+        'd':0,
+        'e':0,
+        'f':0,
+        'g':0,
+        'h':0,
+        'i':0,
+        'j':0,
+        'k':0,
+        'l':0,
+        'm':0,
+        'n':0,
+        'o':0,
+        'p':0,
+        'q':0,
+        'r':0,
+        's':0,
+        't':0,
+        'u':0,
+        'v':0,
+        'w':0,
+        'x':0,
+        'y':0,
+        'z':0
+    }
+    
+    lowered_string = text.lower()
+    
+    for char in lowered_string:
+        if char in char_map:
+            char_map[char] += 1
+    
+    return char_map          
+            
+    
+
 
 main()
 
